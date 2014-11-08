dc5d4ec153e95121f93714dfe407d95f2931822d
David Maitland <hello@davidmaitland.me>
2014-11-08 16:03:52 +0000

    Forgot to rename old test

--- tests/unit/controllers/suggestion-test.js
+++ tests/unit/controllers/suggestion-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('controller:feature', 'FeatureController', {
+moduleFor('controller:suggestion', 'SuggestionController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
