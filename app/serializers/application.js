c8c9529ad2a1dba61a8329ab9a0fbd4ad73e3514
David Maitland <hello@davidmaitland.me>
2014-11-13 23:09:44 +0000

    Extended custom serializer

---
+++ app/serializers/application.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.RESTSerializer.extend({
+
+  serializeIntoHash: function(hash, type, record, options) {
+    Ember.merge(hash, this.serialize(record, options));
+  }
+
+});
