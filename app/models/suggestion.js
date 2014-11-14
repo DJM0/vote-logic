b6f59e6d4659956d57bc3df73c9eda33ae8a17ab
David Maitland <hello@davidmaitland.me>
2014-11-14 23:18:59 +0000

    Added date properties

--- app/models/suggestion.js
+++ app/models/suggestion.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
 
   suggestion: DS.attr('string'),
   description: DS.attr('string'),
-  votes: DS.attr('number', { defaultValue: 0 })
+  votes: DS.attr('number', { defaultValue: 0 }),
+  createdAt: DS.attr('date'),
+  updatedAt: DS.attr('date')
   
 });
