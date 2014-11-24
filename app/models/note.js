ccea62d0b47805be785a166b3e6d3996f34ec490
David Maitland <hello@davidmaitland.me>
2014-11-24 21:48:12 +0000

    Changed vote to note

--- app/models/note.js
+++ app/models/note.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 
   suggestion: DS.belongsTo('suggestion'),
-  votes: DS.attr('number'),
   note: DS.attr('string'),
   email: DS.attr('string'),
   createdAt: DS.attr('date'),
