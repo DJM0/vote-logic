2a4a6fb501817a952a8fe1bf198e4c71581d2017
David Maitland <hello@davidmaitland.me>
2014-11-25 20:39:16 +0000

    Moving models about

--- app/models/suggestion.js
+++ app/models/suggestion.js
@@ -5,8 +5,9 @@ export default DS.Model.extend({
   suggestion: DS.attr('string'),
   description: DS.attr('string'),
   email: DS.attr('string'),
+  total: DS.attr('number', { readOnly: true }),
+  votes: DS.hasMany('vote', { async: true }),
   notes: DS.hasMany('note', { async: true }),
-  votes: DS.attr('number', { readOnly: true }),
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date')
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   beforeModel: function() {
+
     this.transitionTo('suggestions');
+
   }
 
 });
