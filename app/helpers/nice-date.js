ddb8db561f1177d59f798c2b86ba5248f8fd1f68
David Maitland <hello@davidmaitland.me>
2014-11-29 00:27:18 +0000

    Added pretty date helper

---
+++ app/helpers/nice-date.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function niceDate(date) {
+  return moment(date).fromNow(); 
+};
+
+export default Ember.Handlebars.makeBoundHelper(niceDate);
---
+++ tests/unit/helpers/nice-date-test.js
@@ -0,0 +1,11 @@
+import {
+  niceDate
+} from 'vote-logic/helpers/nice-date';
+
+module('NiceDateHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = niceDate(42);
+  ok(result);
+});
