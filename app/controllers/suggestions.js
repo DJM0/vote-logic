fb5b17c9d9df283f7c36c1681a26cac56ecebb01
David Maitland <hello@davidmaitland.me>
2014-12-05 13:27:45 +0000

    Added sorting button to suggestions

--- app/controllers/suggestions.js
+++ app/controllers/suggestions.js
@@ -3,6 +3,17 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
 
   sortAscending: false,
-  sortProperties: ['createdAt']
+  sortProperties: ['createdAt'],
+
+  actions: {
+
+    sort: function(property) {
+
+      this.set('sortProperties', [property]);
+      this.set('sortAscending', false);
+
+    }
+
+  }
 
 });
