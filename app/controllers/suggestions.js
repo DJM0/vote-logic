4feee5ad1ff4bbe9593b6cbd932c86728d785213
David Maitland <hello@davidmaitland.me>
2014-12-29 16:17:59 +0000

    Make current sort option active

--- app/controllers/suggestions.js
+++ app/controllers/suggestions.js
@@ -5,6 +5,26 @@ export default Ember.ArrayController.extend({
   sortAscending: false,
   sortProperties: ['createdAt'],
 
+  createdAtSort: function() {
+
+    if (this.get('sortProperties')[0] === 'createdAt') {
+      return 'btn btn-default active';
+    } else {
+      return 'btn btn-default';
+    }
+
+  }.property('sortProperties'),
+
+  totalSort: function() {
+
+    if (this.get('sortProperties')[0] === 'total') {
+      return 'btn btn-default active';
+    } else {
+      return 'btn btn-default';
+    }
+
+  }.property('sortProperties'),
+
   actions: {
 
     sort: function(property) {
